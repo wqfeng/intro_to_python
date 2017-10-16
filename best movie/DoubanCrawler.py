780a99349d9e8c24bacab04cd8a582373b352054
Wang Qunfeng <qunfengw@gmail.com>
2017-10-16 14:54:11 +0800

    Update DoubanCrawler.py

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -65,52 +65,40 @@ def writeToCSV():
     for c in categories:
         for l in locations:
             movies += getMovies(c, l)
-    # movies = getMovies('剧情', '美国')
 
     with open('movies.csv', 'w', encoding='utf-8') as csvfile:
         movies_writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for m in movies:
             movies_writer.writerow([m.name, m.rate, m.location, m.category, m.info_link, m.cover_link])
 
-def caculate():
-    html_doc = getHtml(url_prefix) # contains all the movies
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    items = soup.find_all('ul', 'category')
-
-    categories_html = list(items[1].children)
-    locations_html = list(items[2].children)
-    categories = [x.find('span').string for x in categories_html]
-    locations = [x.find('span').string for x in locations_html]
-    categories.pop(0)
-    locations.pop(0)
-
+def test():
     with open('output.txt', 'w', encoding='utf-8') as f:
-        for c in categories:
-            # will get all the movies in category c when location is empty
-            total = len(getMovies(c, ''))
-            count_list = []
-            for l in locations:
-                count_list.append((len(getMovies(c, l)), l))
+        with open('movies.csv', 'r', encoding='utf-8') as csvfile:
+            records = list(csv.reader(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL))
+            categories = set([x[3] for x in records])
+            locations = set([x[2] for x in records])
 
-            count_list.sort(reverse=True)
-            first_count, first_location = count_list[0]
-            second_count, second_location = count_list[1]
-            third_count, third_location = count_list[2]
-
-            f.write('----------------------------------\n')
-            f.write('豆瓣共收录{}片 {} 部，数量排名前三的地区为{}、{}、{}，依次占比 {}%、{}%、{}%。\n'.format(
+            for c in categories:
+                movies_in_c = [m for m in records if m[3] == c]
+                count_by_location = []
+                for l in locations:
+                    count_by_location.append((l, len([m for m in movies_in_c if m[2] == l])))
+                
+                sorted_list = sorted(count_by_location, key=lambda x: x[1], reverse=True)
+                first_location, first_count = sorted_list[0]
+                second_location, second_count = sorted_list[1]
+                third_location, third_count = sorted_list[2]
+                total = len(movies_in_c)
+                print(c)
+                print('豆瓣共收录{}片 {} 部，数量排名前三的地区为{}、{}、{}，依次占比 {:.2%}、{:.2%}、{:.2%}。\n'.format(
+                    c, total, first_location, second_location, third_location,
+                    first_count / total, second_count / total, third_count / total))
+            
+                f.write('豆瓣共收录{}片 {} 部，数量排名前三的地区为{}、{}、{}，依次占比 {:.2%}、{:.2%}、{:.2%}。\n'.format(
                 c, total, first_location, second_location, third_location,
-                first_count * 100 / total, second_count * 100 / total, third_count * 100 / total))
-            f.write('----------------------------------\n')
-
-
+                first_count / total, second_count / total, third_count / total))
 
 
 if __name__ == '__main__':
-    # movies = getMovies('剧情', '美国')
-    writeToCSV()
-    # caculate()
-
-
-
-
+    # writeToCSV()
+    test()
